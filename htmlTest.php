0b3fab9a2810605b1d3b980d3387a8d94d77d517
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-09-30 00:28:23 -0400

    Create htmlTest.php

---
+++ htmlTest.php
@@ -0,0 +1,16 @@
+<p>Run me in the browser from your server to try</p>
+<form method="POST">
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email" required/>
+  <label for="p1">Password:</label>
+  <input type="password" id="p1" name="password" required/>
+  <label for="p2">Confirm Password:</label>
+  <input type="password" id="p2" name="confirm" required/>
+  <input type="submit" name="register" value="Register"/>
+</form>
+
+<?php
+
+echo "I hope this shows up";
+
+?>
