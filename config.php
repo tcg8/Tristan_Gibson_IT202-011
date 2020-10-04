6cee0d67213bc4b2375cf52927ecb4fcf5ade621
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-10-04 14:16:35 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 //for everyone else fill in these variable strings with the connection
 //details from your respective servers and delete the Heroku block above
 $dbhost = "localhost";//this should be fine for all but NJIT, which uses "sql1.njit.edu" or "sql2.njit.edu" or "sql3.njit.edu"
-$dbuser = "ucid";//your ucid since we follow this pattern
-$dbpass = "";//database password generated or chosen per respective steps
-$dbdatabase = "ucid";//your ucid since we follow this pattern
+$dbuser = "tcg8";//your ucid since we follow this pattern
+$dbpass = "Password";//database password generated or chosen per respective steps
+$dbdatabase = "tcg8";//your ucid since we follow this pattern
 ?>
