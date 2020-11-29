c5f096f1334271951a1ef4366ad0cfac3fb10dcb
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-11-29 18:05:15 -0500

    Update db.php

--- project/lib/db.php
+++ project/lib/db.php
@@ -11,7 +11,7 @@ function getDB(){
     if(!isset($db)) {
         try{
             //__DIR__ helps get the correct path regardless of where the file is being called from
-            //it gets the absolute path to this file, then we append the relative url (so up a directory and insid>
+            //it gets the absolute path to this file, then we append the relative url (so up a directory and inside lib)
             require_once(__DIR__. "/config.php");//pull in our credentials
             //use the variables from config to populate our connection
             $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
