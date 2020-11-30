998d615ed768002f1c431e772d12f4e52b30b1e6
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-11-30 12:53:18 -0500

    Update profile.php

--- project/profile.php
+++ project/profile.php
@@ -74,6 +74,7 @@ if (isset($_POST["saved"])) {
         }
         //password is optional, so check if it's even set
         //if so, then check if it's a valid reset request
+        flash($_SESSION["user"]["password"]);
         if (!empty($_POST["password"]) && !empty($_POST["confirm"]) && !empty($_POST["current"])) {
             if (($_POST["password"] == $_POST["confirm"]) ){//&& ($_POST["confirm"] == ____)) { flash($_SESSION["user"]["password"])
                 $password = $_POST["password"];
