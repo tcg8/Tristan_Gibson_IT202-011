960a4d11436e0e83ba1662f96a0173732bc9eed4
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-02 18:48:17 -0500

    Update register.php

--- project/register.php
+++ project/register.php
@@ -76,10 +76,10 @@ if (!isset($username)) {
         <td><input class="form-control" type="email" id="email" name="email" required value="<?php safer_echo($email); ?>"/></td>
     </tr><tr>
         <td><label for="user">Username:</label></td>
-        <td><input class="form-control" type="text" id="user" name="username" required maxlength="60" required minlength="4" value="<?php safer_echo($username); ?>"/></td>
+        <td><input class="form-control" type="text" id="user" name="username" required maxlength="60" value="<?php safer_echo($username); ?>"/></td>
     </tr><tr>
         <td><label for="p1">Password:</label></td>
-        <td><input class="form-control" type="password" id="p1" name="password" required/></td>
+        <td><input class="form-control" type="password" id="p1" required minlength="4" required maxlength="60" name="password" required/></td>
     </tr><tr>
         <td><label for="p2">Confirm Password:</label></td>
         <td><input class="form-control" type="password" id="p2" name="confirm" required/></td>
