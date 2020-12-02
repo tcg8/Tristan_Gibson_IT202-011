b309a814235742b710f3adb4c95952400ffeb227
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-02 18:16:35 -0500

    Update profile.php

--- project/profile.php
+++ project/profile.php
@@ -159,26 +159,26 @@ if (isset($_POST["saved"])) {
         <table style="width:100%">
             <tr>
         <td>  <label for="email">Email</label>  </td>
-        <td>  <input type="email" name="email" value="<?php safer_echo(get_email()); ?>"/>  </td>
+        <td>  <input class="form-control" type="email" name="email" value="<?php safer_echo(get_email()); ?>"/>  </td>
             </tr><tr>
         <td>  <label for="username">Username</label>  </td>
-        <td>  <input type="text" maxlength="60" name="username" value="<?php safer_echo(get_username()); ?>"/>  </td>
+        <td>  <input class="form-control" type="text" maxlength="60" name="username" value="<?php safer_echo(get_username()); ?>"/>  </td>
             </tr><tr>
 
         <!-- DO NOT PRELOAD PASSWORD-->
 
-        <td>  <label for="curpw">Current Password</label>  </td>
+        <td>  <label for="pwc">Current Password</label>  </td>
         <td>  <input id="pwc" class="form-control" type="password" name="current_password"/>  </td>
             </tr><tr>
         <td>  <label for="pw">New Password</label>  </td>
-        <td>  <input type="password" name="password"/>  </td>
+        <td>  <input id="pw" class="form-control" type="password" name="password"/>  </td>
             </tr><tr>
         <td>  <label for="cpw">Confirm Password</label>  </td>
         <td>  <input type="password" name="confirm"/>  </td>
             </tr>
         </table>
         
-        <input type="submit" name="saved" value="Save Profile"/>
+        <input class="btn btn-primary" type="submit" name="saved" value="Save Profile"/>
     </form>
 
 <?php require(__DIR__ . "/partials/flash.php");
