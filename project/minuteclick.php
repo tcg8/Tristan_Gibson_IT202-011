602940c1b56f2f731b6e1a170923c2eeb3718371
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-07 20:48:29 -0500

    Update minuteclick.php

--- project/minuteclick.php
+++ project/minuteclick.php
@@ -14,7 +14,7 @@ flash("You are logged in, your score will be saved");
 //$score=localStorage.clickcount;
 	//$testvar=3;
 	//flash("testing display " . $testvar);
-	$score=$_POST["clicker"];//localStorage.clickcount;
+	$score=$_POST["sendscore"];//localStorage.clickcount;
 	flash("the score is " . $score);
 	
 	if (isset($_POST["clicker"])) {
@@ -48,13 +48,14 @@ if (isset($_POST["sendscore"])) {
 ?>
 
 
-<!--<form>
+<form>
 	<h3>Game starts when you click the button, You have a minute to get a high score!</h3>
 	<button  id="clicker" type="button" style="width: 100%; height: 200px;">Click Me!</button>
 	<div id="result"></div>
 	<input class="btn btn-primary" type="submit" name="sendscore" value="Submit Score"/>
-</form>-->
+</form>
 
+<!--
 <html>
 <head>
 <script>
@@ -86,14 +87,14 @@ function submitScore() {
 	<button onclick="clickCounter()" id="clicker" type="button"  name="clicker" style="width: 100%; height: 200px;" >Click Me!</button>
 	<div id="result"></div>
 	<!--<button onclick="submitScore()" id="sendscore" type="button">Submit Score</button>-->
-	<input class="btn btn-primary" onclick="submitScore()" type="submit" name="sendscore" value="Submit Score" />
+<!--<input class="btn btn-primary" onclick="submitScore()" type="submit" name="sendscore" value="Submit Score" />
 	<!--<button class="btn btn-primary" id="reset" type="button" style>Restart</button>
 	<h3 id="countdown">Time Left</h3>-->
-	</form>
+	<!--</form>
 </body>
 	
 
-</html>
+</html>-->
 
 <?php require(__DIR__ . "/partials/flash.php");
 	
