1328cfb30be866767e45baa043594184ff87937a
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-08 18:36:29 -0500

    Update nav.php

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -11,6 +11,9 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
+    <?php if (has_role("Admin")): ?>
+            <li><a href="admin_only.php">Admin Page</a></li>
+        <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
