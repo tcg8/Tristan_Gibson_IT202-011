953382d72ba7ee2ed920b5b4c6083f7fd15de59a
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-07 19:35:20 -0500

    Update minuteclick.php

--- project/minuteclick.php
+++ project/minuteclick.php
@@ -32,9 +32,6 @@ if (isset($_POST["sendscore"])) {
 
 
 
-<html>
-<head>
-<script>
 function clickCounter() {
   if (typeof(Storage) !== "undefined") {
     if (localStorage.clickcount) {
@@ -53,20 +50,18 @@ function submitScore() {
  localStorage.clickcount = 0;
  document.getElementById("result").innerHTML = "Your current score is " + 0;
 }
-</script>
-</head>
+
 	
-<body>
+
 	<h3>Game starts when you click the button, You have a minute to get a high score!</h3>
 	<button onclick="clickCounter()" id="clicker" type="button" style="width: 100%; height: 200px;">Click Me!</button>
 	<div id="result"></div>
 	<button onclick="submitScore()" id="sendscore" type="button">Submit Score</button>
 	<!--<button class="btn btn-primary" id="reset" type="button" style>Restart</button>
 	<h3 id="countdown">Time Left</h3>-->
-</body>
+
 	
 
-</html>
 
 <?php require(__DIR__ . "/partials/flash.php");
 	
