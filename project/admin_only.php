64abe7eb56f450a99e712fbbde668db3bd266539
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-08 13:13:14 -0500

    Create admin_only.php

---
+++ project/admin_only.php
@@ -0,0 +1,14 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+
+<h3>Only the admin should be able to see this page.<h3>
+
+
+
+<?php require(__DIR__ . "/partials/flash.php");
