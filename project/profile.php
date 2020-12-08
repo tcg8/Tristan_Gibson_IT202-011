56e0ec39e3e1a54ff5b9ae9c9920e47459791978
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-08 01:00:45 -0500

    Update profile.php

--- project/profile.php
+++ project/profile.php
@@ -14,8 +14,8 @@ if (!is_logged_in()) {
 
 //<?php
 
-$stmt = $db->prepare("
-SELECT Scores.score FROM Scores JOIN Users on Scores.user_id = Users.id where Users.id = :user_id");// and Roles.is_active = 1 and UserRoles.is_active = 1");
+//$stmt = $db->prepare("
+//SELECT Scores.score FROM Scores JOIN Users on Scores.user_id = Users.id where Users.id = :user_id");// and Roles.is_active = 1 and UserRoles.is_active = 1");
 //$stmt = $db->prepare("
 //SELECT Roles.name FROM Roles JOIN UserRoles on Roles.id = UserRoles.role_id where UserRoles.user_id = :user_id and Roles.is_active = 1 and UserRoles.is_active = 1");
 
