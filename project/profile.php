a8f2038ee9085aa046e0277306c08655c29b192c
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-08 00:33:33 -0500

    Update profile.php

--- project/profile.php
+++ project/profile.php
@@ -9,6 +9,9 @@ if (!is_logged_in()) {
     die(header("Location: login.php"));
 }
 
+$testingThing=$_SESSION["user"]["email"];
+flash("testing thisThing " . $testingThing );
+
 $db = getDB();
 //save data if we submitted the form
 if (isset($_POST["saved"])) {
