6a545f9aaae748afc678c290e250ba3ff2248132
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-07 19:04:41 -0500

    Update nav.php

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -6,6 +6,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
 <nav>
 <ul class="nav">
     <li><a href="home.php">Home</a></li>
+    <li><a href="minuteclick.php">Game</a></li>
     <?php if (!is_logged_in()): ?>
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
