3c83dc07c5dd48db428cd95d42acb2d5df52153a
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-07 19:43:37 -0500

    Update minuteclick.php

--- project/minuteclick.php
+++ project/minuteclick.php
@@ -12,18 +12,15 @@ if (isset($_POST["sendscore"])) {
             $stmt = $db->prepare("INSERT INTO Scores(id, user_id, score, created) VALUES(:id,:user_id,:score,:created)");
             //here's the data map for the parameter to data
             /$params = array(":id" => id, ":user_id" => $user_id, ":score" => $score, ":created" => $created);
-            $r = $stmt->execute($params);
-            $e = $stmt->errorInfo();
+            
+	    $r = $stmt->execute($params);
+            
+	    $e = $stmt->errorInfo();
             if ($e[0] == "00000") {
                 flash("Successfully registered! Please login.");
             }
             else {
-                if ($e[0] == "23000") {//code for duplicate entry
-                    flash("Username or email already exists.");
-                }
-                else {
-                    flash("An error occurred, please try again");
-                }
+                flash("something went wrong");
             }*/
         }
 }
@@ -32,6 +29,10 @@ if (isset($_POST["sendscore"])) {
 
 
 
+
+<html>
+<head>
+<script>
 function clickCounter() {
   if (typeof(Storage) !== "undefined") {
     if (localStorage.clickcount) {
@@ -50,18 +51,20 @@ function submitScore() {
  localStorage.clickcount = 0;
  document.getElementById("result").innerHTML = "Your current score is " + 0;
 }
-
+</script>
+</head>
 	
-
+<body>
 	<h3>Game starts when you click the button, You have a minute to get a high score!</h3>
 	<button onclick="clickCounter()" id="clicker" type="button" style="width: 100%; height: 200px;">Click Me!</button>
 	<div id="result"></div>
 	<button onclick="submitScore()" id="sendscore" type="button">Submit Score</button>
 	<!--<button class="btn btn-primary" id="reset" type="button" style>Restart</button>
 	<h3 id="countdown">Time Left</h3>-->
-
+</body>
 	
 
+</html>
 
 <?php require(__DIR__ . "/partials/flash.php");
 	
