980f97e9855deb061f175a12405aac832e520e0d
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-07 20:35:41 -0500

    Update minuteclick.php

--- project/minuteclick.php
+++ project/minuteclick.php
@@ -18,6 +18,7 @@ flash("You are logged in, your score will be saved");
 	}
 if (isset($_POST["sendscore"])) {
 	flash("33This should appear when submit score is clicked");
+	flash("22This should appear when submit score is clicked");
  $db = getDB();
 	if (isset($db)) {
 		flash("1This should appear when submit score is clicked");
