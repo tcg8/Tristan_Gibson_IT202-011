4484bb5460ebbb457ef1845e42b174108abf8246
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-08 22:26:00 -0500

    Update profile.php

--- project/profile.php
+++ project/profile.php
@@ -150,8 +150,7 @@ flash("line 145 " . count($results));
 //ADD A FOR LOOP HERE TO CREATE THE TOP 10 CHART    USE ECHO OR FLASH   TO CREATE THE CHART
 $i = count($results);
 do {
-  echo "a is $results[0] <br>\n";
-    echo "a is $results[1] <br>\n";
+    flash("a is " . $results[1]);
   $i--;
 }
 while($i>0);
