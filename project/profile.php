bb5f7c2555cdf3645c597215f0d31852522b3480
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-08 20:25:47 -0500

    Update profile.php

--- project/profile.php
+++ project/profile.php
@@ -142,7 +142,7 @@ if (isset($_POST["saved"])) {
 $stmt = $db->prepare("SELECT * from Scores where user_id = :id order by created desc limit 10");
 $params = array(":id" => get_user_id());
 $r = $stmt->execute($params);
-flash("line 145 " . $r);
+flash("line 145 " . count($params));
 
 //$stmt = $db->prepare("SELECT password from Users WHERE id = :id LIMIT 1");
 //flash("check somethin " . :id);
