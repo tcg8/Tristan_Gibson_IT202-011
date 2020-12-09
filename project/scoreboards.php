35ec3b8adde4239bf2628e0102b50dfe83f6a3d8
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-09 13:51:47 -0500

    Update scoreboards.php

--- project/scoreboards.php
+++ project/scoreboards.php
@@ -1,16 +1,7 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 
 
-<?php
-//Note: we have this up here, so our update happens before our get/fetch
-//that way we'll fetch the updated data and have it correctly reflect on the form below
-//As an exercise swap these two and see how things change
-if (!is_logged_in()) {
-    //this will redirect to login and kill the rest of this script (prevent it from executing)
-    flash("You must be logged in to access this page");
-    die(header("Location: login.php"));
-}
-?>
+
 
 <?php
 $db = getDB();
