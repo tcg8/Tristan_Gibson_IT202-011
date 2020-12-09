81c155bd3aba8d0f7e2f84a5f2e9fa5592b8b2b5
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-09 14:05:15 -0500

    Update scoreboards.php

--- project/scoreboards.php
+++ project/scoreboards.php
@@ -1,24 +1,21 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 
 
-
-
 <?php
 $db = getDB();
 $stmt = $db->prepare("SELECT score,created from Scores where created >= :timeCon order by score desc limit 10");
 $stmt = $db->prepare("SELECT score from Scores where created >= :timeCon order by score desc limit 10");
 //WILL NEED A WHERE STATEMENT TO GET TIME FRAME
-//$params = array(":id" => get_user_id());
-$testtime=strtotime("-1 Month");
+$testtime=strtotime("-1 Year");
 $params = array(":timeCon" => date("Y-m-d h:i:s", $testtime));
 $results = $stmt->execute($params);
 $results = $stmt->fetchAll();
-//flash("array length check " . count($results));
-//flash("todays date is " . );
-/*$currtime=time();//$currtime=mktime(11, 14, 54, 8, 12, 2014);
+/*
+$currtime=time();//$currtime=mktime(11, 14, 54, 8, 12, 2014);
 $testtime=strtotime("-1 Month");//strtotime("-1 Weeks");//strtotime("-1 Years");
 flash( "Current date is " . date("Y-m-d h:i:s", $currtime));
-flash( "Testing date is " . date("Y-m-d h:i:s", $testtime));*/
+flash( "Testing date is " . date("Y-m-d h:i:s", $testtime));
+*/
 
 
 //ADD A FOR LOOP HERE TO CREATE THE TOP 10 CHART    USE ECHO OR FLASH   TO CREATE THE CHART
