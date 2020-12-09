6658d35b613ea210610141edebd7ecae1ca9b4d8
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-09 12:13:58 -0500

    Update profile.php

--- project/profile.php
+++ project/profile.php
@@ -150,26 +150,36 @@ $results = $stmt->fetchAll();
 flash("line 150 " . count($results));
 
 //ADD A FOR LOOP HERE TO CREATE THE TOP 10 CHART    USE ECHO OR FLASH   TO CREATE THE CHART
-flash("Your last 10 scores (still need message for when user has less than 10 scores)");
-$i = count($results)-10;
-$a=1;
-do {
-    //So $results was printing double, like 27 came out at 2727 and 0 as 00 so I am modding by 10^(number of digits / 2)
-    //so when $results is 2727 we do $results%$modifier where $modifier will be 100 since 27 is a 2 digit number and 10^2=100
-    //doing modifier like this will get rid of the extra digits being produced
-    //$numlength = strlen((string)$num);
-    $numlength = strlen(implode($results[$i]))/2; //this gets the number of digits that is supposed to be printed
-    $modifier = 10**$numlength;//this is the number that $results will be modified by, it just gets 10^power of $numlength
-    $finalNum = implode($results[$i]) % $modifier;
-    //flash("digits " . length(implode($results[$i])));
-    //if(implode($results[$i])>10){$check=100;}
-    flash("#" . $a . " most recent score is " . $finalNum);
-    //flash("#" . $a . " most recent score is " . implode($results[$i]) . " finalNum " . $finalNum);//%$check);//for some reason the score displayed is being doubled
-  $a++;
-  $i++;
+$hasScores=true;
+if (count($results)==0) {
+    $hasScores=false;
+    flash("You do not have any scores recorded, try playing the game!");
 }
-while($i<10);
+if($hasScores) {
+    flash("Your last 10 scores (still need message for when user has less than 10 scores)");
+    $i=count($results);
+    if(count($results)==10) {
+        $i = count($results)-10;
+    }
+    $a=1;
+    do {
+        //So $results was printing double, like 27 came out at 2727 and 0 as 00 so I am modding by 10^(number of digits / 2)
+        //so when $results is 2727 we do $results%$modifier where $modifier will be 100 since 27 is a 2 digit number and 10^2=100
+        //doing modifier like this will get rid of the extra digits being produced
+        //$numlength = strlen((string)$num);
+        $numlength = strlen(implode($results[$i]))/2; //this gets the number of digits that is supposed to be printed
+        $modifier = 10**$numlength;//this is the number that $results will be modified by, it just gets 10^power of $numlength
+        $finalNum = implode($results[$i]) % $modifier;
+        //flash("digits " . length(implode($results[$i])));
+        //if(implode($results[$i])>10){$check=100;}
+        flash("#" . $a . " most recent score is " . $finalNum);
+        //flash("#" . $a . " most recent score is " . implode($results[$i]) . " finalNum " . $finalNum);//%$check);//for some reason the score displayed is being doubled
+      $a++;
+      $i++;
+    }
+    while($i<10);
 
+}
 ?>
 
 <?php foreach($results as $r):?>
