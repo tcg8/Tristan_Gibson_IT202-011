0677186fbde785d1939bf28972ee3a7f299454db
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-09 12:42:31 -0500

    Update profile.php

--- project/profile.php
+++ project/profile.php
@@ -140,14 +140,11 @@ if (isset($_POST["saved"])) {
 
 <?php
 
-
 $stmt = $db->prepare("SELECT score from Scores where user_id = :id order by id desc limit 10");
 $params = array(":id" => get_user_id());
 $results = $stmt->execute($params);
 $results = $stmt->fetchAll();
-
-//flash("line 145 " . count($results));
-//flash("line 150 " . count($results));
+//flash("array length check " . count($results));
 
 //ADD A FOR LOOP HERE TO CREATE THE TOP 10 CHART    USE ECHO OR FLASH   TO CREATE THE CHART
 $hasScores=true;
@@ -160,26 +157,23 @@ if($hasScores) {
     $i=10-count($results);
     
     if(count($results)==10) {
-        $i=count($results)-10;
+        $i=10-count($results);
     }
     $a=1;
     do {
         //So $results was printing double, like 27 came out at 2727 and 0 as 00 so I am modding by 10^(number of digits / 2)
-        //so when $results is 2727 we do $results%$modifier where $modifier will be 100 since 27 is a 2 digit number and 10^2=100
+        //so when $results is 2727 we do ( $results % $modifier ) where $modifier will be 100 since 27 is a 2 digit number and 10^2=100
         //doing modifier like this will get rid of the extra digits being produced
         //$numlength = strlen((string)$num);
         $numlength = strlen(implode($results[$a-1]))/2; //this gets the number of digits that is supposed to be printed
         $modifier = 10**$numlength;//this is the number that $results will be modified by, it just gets 10^power of $numlength
         $finalNum = implode($results[$a-1]) % $modifier;
-        //flash("digits " . length(implode($results[$i])));
-        //if(implode($results[$i])>10){$check=100;}
         flash("#" . $a . " most recent score is " . $finalNum);
         //flash("#" . $a . " most recent score is " . implode($results[$i]) . " finalNum " . $finalNum);//%$check);//for some reason the score displayed is being doubled
       $a++;
       $i++;
     }
     while($i<10);
-
 }
 ?>
 
