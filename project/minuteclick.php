5df2e2ad9ef62196bc7b84fec28c7c6dd7bd1a79
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-11 16:08:37 -0500

    Update minuteclick.php

--- project/minuteclick.php
+++ project/minuteclick.php
@@ -104,7 +104,30 @@ function clickCounter() {
 	
 	
 
-	
+	function addScore() {
+            //https://www.w3schools.com/xml/ajax_xmlhttprequest_send.asp
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    let json = JSON.parse(this.responseText);
+                    if (json) {
+                        if (json.status == 200) {
+                            alert("Congrats you received 1 " + clickcount);
+                            location.reload();
+                        } else {
+                            alert(json.error);
+                        }
+                    }
+                }
+            };
+            
+            xhttp.open("POST", "<?php echo getURL("minuteclick.php");?>", true);
+	    //this is required for post ajax calls to submit it as a form
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            //map any key/value data similar to query params
+            xhttp.send();
+
+        }
 	
 	
 	
