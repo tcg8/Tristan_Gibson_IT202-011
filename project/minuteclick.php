d255ca0e0be75b9484409c8ea91c71fb027d5091
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-11 17:48:19 -0500

    Update minuteclick.php

--- project/minuteclick.php
+++ project/minuteclick.php
@@ -1,62 +1,11 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
-<?php $huz="wasssssssssssup"; ?>
-<?php
-//This is the game I made for Milestone 2, you click a button as much as possible in one minute for a score. The clock hasn't been added yet but the score still works
-//which is enough for this milestone
-
-//This game is now completely done, Finished for milestone 4
-
-if (!is_logged_in()) {
-    //this will redirect to login and kill the rest of this script (prevent it from executing)
-    flash("You are not logged in, your score won't be saved!");
-    //die(header("Location: login.php"));
-}
-
-
-flash("HUZZA the huz is here " . $huz);
-
-
-	if (isset($_POST["sendscore"])) {
-		
-		$db = getDB();
-        	if (isset($db)) {
-			$user_id = get_user_id();//$_SESSION["user"]["id"];
-			$score = $_POST["count"];//7;
-			//flash("1This should appear when submit score is clicked");
-			///*
-			//here we'll use placeholders to let PDO map and sanitize our data
-			$stmt = $db->prepare("INSERT INTO Scores( user_id, score) VALUES(:user_id,:score)");
-			//here's the data map for the parameter to data
-			$params = array( ":user_id" => $user_id, ":score" => $score);
-			$r = $stmt->execute($params);
-			/*$r = $stmt -> execute([":user_id" => $user_id, ":score" => $score]);
-			if($r){
-				flash("Created successfully with id: " . $db->lastInsertId());
-			}
-			else{
-				$e = $stmt->errorInfo();
-				flash("Error creating :" . var_export($e, true));
-			}
-			*/$e = $stmt->errorInfo();
-			if ($e[0] == "00000") {
-				flash("Successfully recorded score");
-			}
-			else {
-				flash("You are not logged in so the score was not saved");
-			}//*/
-		}
-	}
-?>
-
-
-
 
 
 <html>
 <head>
 <script>
 
-var constTime=20;   //this is so I can change the time length of the game easier, maily for testing
+var constTime=3;   //this is so I can change the time length of the game easier, maily for testing
 var constTime2=5;   //this is for cooldown timer
 
 //variables for game and game timer
@@ -131,6 +80,7 @@ function startTimer(){
            clearInterval(timer);
 	       //Start cooldown now that game is over
 	       onCooldown=true;
+	       testy.value=3;
 	       startCooldown();
 	       
        }//---if(time<=0)
@@ -175,11 +125,70 @@ function startCooldown(){
 	<button onclick="clickCounter()" id="clicker" type="button"  name="clicker" style="width: 100%; height: 200px;" >Click Me!</button>
 	<div id="result"></div>
 		<input type="hidden" id="count" name="count" value=0 />
+		<input type="hidden" id="testy" name="testy" />
 	<input class="btn btn-primary" onclick="submitScore()" type="submit" name="sendscore" value="Submit Score" />
 	<!----></form>
 </body>
 	
 
 </html>
+
+
+
+
+
+<?php
+//This is the game I made for Milestone 2, you click a button as much as possible in one minute for a score. The clock hasn't been added yet but the score still works
+//which is enough for this milestone
+
+//This game is now completely done, Finished for milestone 4
+
+if (!is_logged_in()) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You are not logged in, your score won't be saved!");
+    //die(header("Location: login.php"));
+}
+
+if(isset($_POST["testy"])){
+flash("HUZZA the huz is here ");// . $huz);
+}
+
+
+	if (isset($_POST["sendscore"])) {
+		
+		$db = getDB();
+        	if (isset($db)) {
+			$user_id = get_user_id();//$_SESSION["user"]["id"];
+			$score = $_POST["count"];//7;
+			//flash("1This should appear when submit score is clicked");
+			///*
+			//here we'll use placeholders to let PDO map and sanitize our data
+			$stmt = $db->prepare("INSERT INTO Scores( user_id, score) VALUES(:user_id,:score)");
+			//here's the data map for the parameter to data
+			$params = array( ":user_id" => $user_id, ":score" => $score);
+			$r = $stmt->execute($params);
+			/*$r = $stmt -> execute([":user_id" => $user_id, ":score" => $score]);
+			if($r){
+				flash("Created successfully with id: " . $db->lastInsertId());
+			}
+			else{
+				$e = $stmt->errorInfo();
+				flash("Error creating :" . var_export($e, true));
+			}
+			*/$e = $stmt->errorInfo();
+			if ($e[0] == "00000") {
+				flash("Successfully recorded score");
+			}
+			else {
+				flash("You are not logged in so the score was not saved");
+			}//*/
+		}
+	}
+?>
+
+
+
+
+
 
 <?php require(__DIR__ . "/partials/flash.php");
