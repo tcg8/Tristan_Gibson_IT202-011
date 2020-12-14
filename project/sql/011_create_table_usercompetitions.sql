1a0b90524b28391d5fe2a87e372ec791e371371e
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-14 09:40:26 -0500

    Update 011_create_table_usercompetitions.sql

--- project/sql/011_create_table_usercompetitions.sql
+++ project/sql/011_create_table_usercompetitions.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS  `UserCompetitions`
 (
-    `id`         int auto_increment not null,
-    `competition_id` int,
-    `user_id` int,
-    `created`    timestamp default current_timestamp,
-    `modified`   timestamp default current_timestamp on update current_timestamp,
+    `id`                int auto_increment not null,
+    `competition_id`    int,
+    `user_id`           int,
+    `created`           timestamp default current_timestamp,
+    `modified`          timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES Users(`id`),
     FOREIGN KEY (`competition_id`) REFERENCES Competitions(`id`),
