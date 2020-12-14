2d35b2ad04889acc3bc0245549bed198db190119
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-14 11:52:49 -0500

    Update helpers.php

--- project/lib/helpers.php
+++ project/lib/helpers.php
@@ -139,7 +139,7 @@ if($hasScores) {
         $numlength = strlen(implode($results2[$a-1]))/2; //this gets the number of digits that is supposed to be printed
         $modifier = 10**$numlength;//this is the number that $results will be modified by, it just gets 10^power of $numlength
         $userbro = substr(implode($results2[$a-1]),0,$numlength);// % $modifier;
-$userbro="<a href="login.php">$userbro</a>"
+$userbro="<a href='profile.php'>$userbro</a>"
 //echo '<a href="mycgi?foo=', urlencode($userbro), '">';
         $numlength = strlen(implode($results3[$a-1]))/2; //this gets the number of digits that is supposed to be printed
         $modifier = 10**$numlength;//this is the number that $results will be modified by, it just gets 10^power of $numlength
