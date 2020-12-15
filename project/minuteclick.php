61cf2f84a01e109c4e9692e69ffe8860aeaff4c8
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-15 12:40:15 -0500

    Update minuteclick.php

--- project/minuteclick.php
+++ project/minuteclick.php
@@ -39,6 +39,9 @@ if (!is_logged_in()) {
             		$params = array(":id" => get_user_id());
             		$r = $stmt->execute($params);
 			
+			//This is just to update the query
+			$stmt = $db->prepare("SELECT id, email, username, password, points from Users WHERE email = :email LIMIT 1");
+			
 			
 			$e = $stmt->errorInfo();
 			if ($e[0] == "00000") {
