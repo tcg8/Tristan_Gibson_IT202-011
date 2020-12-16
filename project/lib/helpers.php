a4cec8966bc3762816439a15555cd40a69701d20
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-16 18:42:32 -0500

    Update helpers.php

--- project/lib/helpers.php
+++ project/lib/helpers.php
@@ -217,6 +217,32 @@ foreach($results as $r):
 endforeach;
 }
 
+function paginate($query, $params = [], $per_page = 10) {
+    global $page;
+    if (isset($_GET["page"])) {
+        try {
+            $page = (int)$_GET["page"];
+        }
+        catch (Exception $e) {
+            $page = 1;
+        }
+    }
+    else {
+        $page = 1;
+    }
+    $db = getDB();
+    $stmt = $db->prepare($query);
+    $stmt->execute($params);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $total = 0;
+    if ($result) {
+        $total = (int)$result["total"];
+    }
+    global $total_pages;
+    $total_pages = ceil($total / $per_page);
+    global $offset;
+    $offset = ($page - 1) * $per_page;
+}
 
 
 
