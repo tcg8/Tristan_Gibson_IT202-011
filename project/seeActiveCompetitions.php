f7ad9e16f58a39dd6bb1c42f5e83a4df40708c90
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-15 20:26:53 -0500

    Update seeActiveCompetitions.php

--- project/seeActiveCompetitions.php
+++ project/seeActiveCompetitions.php
@@ -7,7 +7,7 @@ if (isset($_POST["join"])) {
     //prevent user from joining expired or paid out comps
     //$stmt = $db->prepare("select fee from Competitions where id = :id && expires > current_timestamp && paid_out = 0");
     $stmt = $db->prepare("select fee from Competitions where expires > current_timestamp && paid_out = 0");
-    $r = $stmt->execute([":id" => $_POST["cid"]]);
+    $r = $stmt->execute();//[":id" => $_POST["cid"]]
     if ($r) {
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
         if ($result) {
