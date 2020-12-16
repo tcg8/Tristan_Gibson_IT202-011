0a28afb7abbf0c863c5314451f7c928a3900c51d
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-16 14:03:22 -0500

    Update seeActiveCompetitions.php

--- project/seeActiveCompetitions.php
+++ project/seeActiveCompetitions.php
@@ -164,9 +164,9 @@ else {
 					$r3 = $stmt3->execute([":id" => get_user_id()]);
 					if ($r3) {
 					    $results3 = $stmt3->fetchAll(PDO::FETCH_ASSOC);
-						flash("howdy");
+						flash("howdy " . $r3["c.competition_id"] . "    also " . $r3["competition_id"] );
 					}//*/
-					flash("ayyyyy lets see it: " . $results3["competition_id"]);
+					flash("ayyyyy lets see it: " . $results3["c.competition_id"]);
 					
 				 ?>
 			</div>
