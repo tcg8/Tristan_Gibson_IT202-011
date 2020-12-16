2ec09143a34c8137ac517e0ce0b9b804cf781737
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-16 18:34:42 -0500

    Update seeActiveCompetitions.php

--- project/seeActiveCompetitions.php
+++ project/seeActiveCompetitions.php
@@ -9,6 +9,50 @@ if (!is_logged_in()) {
 ?>
 
 <?php
+$page = 1;
+$per_page = 10;
+if(isset($_GET["page"])){
+    try {
+        $page = (int)$_GET["page"];
+    }
+    catch(Exception $e){
+
+    }
+}
+
+//$stmt = $db->prepare("SELECT count(*) as total from F20_Eggs e LEFT JOIN F20_Incubators i on e.id = i.egg_id where e.user_id = :id");
+//$stmt = $db->prepare("select fee from Competitions where id = :id && expires > current_timestamp && paid_out = 0 LIMIT 10");
+$stmt = $db->prepare("select count(*) as total from Competitions where id = :id && expires > current_timestamp && paid_out = 0 LIMIT 10");
+$stmt->execute([":id"=>get_user_id()]);
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$total = 0;
+if($result){
+    $total = (int)$result["total"];
+}
+$total_pages = ceil($total / $per_page);
+$offset = ($page-1) * $per_page;
+
+
+//$stmt = $db->prepare("SELECT e.*, i.name as inc from F20_Eggs e LEFT JOIN F20_Incubators i on e.id = i.egg_id where e.user_id = :id LIMIT :offset, :count");
+//$stmt = $db->prepare("SELECT c.* FROM Competitions c WHERE c.expires > current_timestamp AND paid_out = 0 ORDER BY expires ASC LIMIT 10");//Use this one or you can only see what you created
+$stmt = $db->prepare("SELECT c.* FROM Competitions c WHERE c.expires > current_timestamp AND paid_out = 0 ORDER BY expires ASC LIMIT :offset, :count");//Use this one or you can only see what you created
+
+//need to use bindValue to tell PDO to create these as ints
+//otherwise it fails when being converted to strings (the default behavior)
+$stmt->bindValue(":offset", $offset, PDO::PARAM_INT);
+$stmt->bindValue(":count", $per_page, PDO::PARAM_INT);
+$stmt->bindValue(":id", get_user_id());
+$stmt->execute();
+
+$e = $stmt->errorInfo();
+if($e[0] != "00000"){
+    flash(var_export($e, true), "alert");
+}
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php
 
 $db = getDB();
 
