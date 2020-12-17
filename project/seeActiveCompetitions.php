33112bda180439fc0e567d53ac731825aba465ff
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-16 19:28:48 -0500

    Update seeActiveCompetitions.php

--- project/seeActiveCompetitions.php
+++ project/seeActiveCompetitions.php
@@ -185,6 +185,21 @@ else {
 
 ?>
 
+</div>
+<ul class="pagination justify-content-center">
+	<li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
+	    <a class="page-link" href="?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
+	</li>
+	<?php for($i = 0; $i < $total_pages; $i++):?>
+		<li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
+	<?php endfor; ?>
+	<li class="page-item <?php echo ($page) >= $total_pages?"disabled":"";?>">
+		<a class="page-link" href="?page=<?php echo $page+1;?>">Next</a>
+	</li>
+</ul>
+</div>
+
+
 <div class="container-fluid">
         <h3>Active Competitions</h3>
         <div class="list-group">
