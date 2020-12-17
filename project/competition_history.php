a05d99bb75b627dad72d0045c7afa9dd7b72de7b
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-17 14:27:01 -0500

    Update competition_history.php

--- project/competition_history.php
+++ project/competition_history.php
@@ -23,7 +23,7 @@ paginate($query, [], $per_page);
 
 //$stmt = $db->prepare("SELECT * FROM Scores WHERE user_id = :id ORDER BY created DESC LIMIT :offset,:count");
 //$stmt = $db->prepare("SELECT * FROM UserCompetitions WHERE user_id = :id ORDER BY created DESC LIMIT :offset,:count");
-$stmt = $db->prepare("SELECT u.* FROM UserCompetitions u LEFT JOIN Competitions c ON c.id=u.competition_id WHERE u.user_id = $theID ORDER BY created DESC LIMIT :offset,:count");
+$stmt = $db->prepare("SELECT u.*,c.name FROM UserCompetitions u LEFT JOIN Competitions c ON c.id=u.competition_id WHERE u.user_id = $theID ORDER BY created DESC LIMIT :offset,:count");
 $stmt->bindValue(":offset", $offset, PDO::PARAM_INT);
 $stmt->bindValue(":count", $per_page, PDO::PARAM_INT);
 $stmt->bindValue(":id", get_user_id(), PDO::PARAM_INT);
