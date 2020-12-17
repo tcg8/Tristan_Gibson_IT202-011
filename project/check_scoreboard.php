766a867437a9b54ca6cfac3c1a10a8fecaee82ec
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-17 17:37:51 -0500

    Create check_scoreboard.php

---
+++ project/check_scoreboard.php
@@ -0,0 +1,40 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+
+
+
+//<a href='profile.php'>$userbro</a>
+
+
+<div class="container-fluid">
+        <h3>The Top 10 Scores</h3>
+        <div class="list-group">
+            <?php if (isset($results) && count($results)): ?>
+                <?php foreach ($results as $r): ?>
+                    <div class="list-group-item" style="background-color: #25E418">
+                        <div class="row">
+				
+                            <div class="col">
+                                You scored: 
+                                <?php safer_echo($r["score"]); ?>
+                            </div>
+                            <div class="col">
+                                Scored on: 
+                                <?php safer_echo($r["created"]); ?>
+                            </div>
+			    <div class="col">
+                                <form method="POST">
+				</form>
+                            </div>
+                             
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <div class="list-group-item">
+                    No scores to show, sorry.
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+
+<?php require(__DIR__ . "/partials/flash.php");
