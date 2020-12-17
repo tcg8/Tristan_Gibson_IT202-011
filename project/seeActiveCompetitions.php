06a4b8036dd44de90e18ab8e980a6b5c0cd1b26d
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-16 19:39:13 -0500

    Update seeActiveCompetitions.php

--- project/seeActiveCompetitions.php
+++ project/seeActiveCompetitions.php
@@ -187,7 +187,21 @@ else {
 
 
 
-
+<div>
+	<nav aria-label="My Competitions">
+		<ul class="pagination justify-content-center">
+			<li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
+			    <a class="page-link" href="?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
+			</li>
+			<?php for($i = 0; $i < $total_pages; $i++):?>
+				<li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
+			<?php endfor; ?>
+			<li class="page-item <?php echo ($page) >= $total_pages?"disabled":"";?>">
+				<a class="page-link" href="?page=<?php echo $page+1;?>">Next</a>
+			</li>
+		</ul>
+	</nav>
+</div>
 
 
 <div class="container-fluid">
@@ -242,18 +256,6 @@ else {
     </div>
 
 </div>
-<div>
-<ul class="pagination justify-content-center">
-	<li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
-	    <a class="page-link" href="?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
-	</li>
-	<?php for($i = 0; $i < $total_pages; $i++):?>
-		<li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
-	<?php endfor; ?>
-	<li class="page-item <?php echo ($page) >= $total_pages?"disabled":"";?>">
-		<a class="page-link" href="?page=<?php echo $page+1;?>">Next</a>
-	</li>
-</ul>
-</div>
+
 
 <?php require(__DIR__ . "/partials/flash.php");
