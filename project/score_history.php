33ff95322da8efe8640e1b30fbc8467d8112e702
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-16 22:01:11 -0500

    Update score_history.php

--- project/score_history.php
+++ project/score_history.php
@@ -1,1 +1,22 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
 
+<?php
+if (!is_logged_in()) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+
+
+<?php
+
+$db = getDB();
+//Do this tomorrow morning
+
+?>
+
+
+<?php include(__DIR__ . "/partials/pagination.php");?>
+
+<?php require(__DIR__ . "/partials/flash.php");
