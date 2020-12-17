02b03f76cc52f775a38d5da57fd08a21ac4233a8
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-17 14:28:07 -0500

    Update competition_history.php

--- project/competition_history.php
+++ project/competition_history.php
@@ -16,14 +16,15 @@ $db = getDB();
 $per_page = 10;
 $theID = get_user_id();
 //$query = "SELECT count(*) as total FROM Scores WHERE user_id = $theID ORDER BY created DESC";
-//$query = "SELECT count(*) as total FROM UserCompetitions WHERE user_id = $theID ORDER BY created DESC";
-$query = "SELECT u.count(*) as total FROM UserCompetitions u LEFT JOIN Competitions c ON c.id=u.competition_id WHERE u.user_id = $theID ORDER BY created DESC";
+$query = "SELECT count(*) as total FROM UserCompetitions WHERE user_id = $theID ORDER BY created DESC";
+//$query = "SELECT u.count(*) as total FROM UserCompetitions u LEFT JOIN Competitions c ON c.id=u.competition_id WHERE u.user_id = $theID ORDER BY created DESC";
 paginate($query, [], $per_page);
 
 
 //$stmt = $db->prepare("SELECT * FROM Scores WHERE user_id = :id ORDER BY created DESC LIMIT :offset,:count");
 //$stmt = $db->prepare("SELECT * FROM UserCompetitions WHERE user_id = :id ORDER BY created DESC LIMIT :offset,:count");
 $stmt = $db->prepare("SELECT u.*,c.name FROM UserCompetitions u LEFT JOIN Competitions c ON c.id=u.competition_id WHERE u.user_id = $theID ORDER BY created DESC LIMIT :offset,:count");
+//$stmt = $db->prepare("SELECT e.*, i.name as inc from F20_Eggs e LEFT JOIN F20_Incubators i on e.id = i.egg_id where e.user_id = :id LIMIT :offset, :count");
 $stmt->bindValue(":offset", $offset, PDO::PARAM_INT);
 $stmt->bindValue(":count", $per_page, PDO::PARAM_INT);
 $stmt->bindValue(":id", get_user_id(), PDO::PARAM_INT);
