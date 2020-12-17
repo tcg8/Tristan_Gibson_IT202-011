d391f469fceba0a41b08ce37283ecc9e51d6a79a
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-17 15:55:34 -0500

    Update home.php

--- project/home.php
+++ project/home.php
@@ -6,5 +6,17 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
     $email = $_SESSION["user"]["email"];
 }
 ?>
+
+
     <p>Welcome, <?php echo $email; ?></p>
+
+<?php
+get10week(); 
+get10month(); 
+get10lifetime();
+
+?>
+
+    
+<?php require(__DIR__ . "/partials/flash2.php");?>
 <?php require(__DIR__ . "/partials/flash.php");
