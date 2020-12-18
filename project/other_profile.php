06fa09f68d22ebedfefdd73440a382ae0f38afa6
tcg8 <50150270+tcg8@users.noreply.github.com>
2020-12-17 19:34:28 -0500

    Update other_profile.php

--- project/other_profile.php
+++ project/other_profile.php
@@ -3,11 +3,7 @@
 //Note: we have this up here, so our update happens before our get/fetch
 //that way we'll fetch the updated data and have it correctly reflect on the form below
 //As an exercise swap these two and see how things change
-if (!is_logged_in()) {
-    //this will redirect to login and kill the rest of this script (prevent it from executing)
-    flash("You must be logged in to access this page");
-    die(header("Location: login.php"));
-}
+
 
 
 if(isset($_GET["id"])){
